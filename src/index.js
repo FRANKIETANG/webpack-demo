2bcc0f740d2ace28182ada9139e9387de2eb3a00
FRANKIETANG <350558468@qq.com>
2017-08-20 15:24:53 +0800

    添加 webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function component () {
     var element = $('<div></div>')
 
     /* lodash is required for the next line to work */
-    element.html(_.join(['Hi','webpack'], ' '))
+    element.html(_.join(['Hi','Frankietang'], ' '))
 
     return element.get(0)
 
